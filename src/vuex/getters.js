58c7117dc1c791ef1a5bc1db1cbb54c6cc82cb1d
power_840 <liuyu19921017@gmail.com>
2018-04-26 22:26:44 +0800

    issue

--- src/vuex/getters.js
+++
@@ -1,8 +0,0 @@
-const getters = {
-    corpid: state => state.common.corpid,   // 企业cropid
-    corpsecret: state => state.common.corpsecret,   // 应用
-    access_token: state => state.common.access_token,   // 企业cropid
-    isLoading: state => state.common.isLoading,   // 企业cropid
-    alertShow: state => state.common.alertShow,   // 企业cropid
-}
-export default getters
